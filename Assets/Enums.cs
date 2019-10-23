52229b1efbc8ab2b77176e2d94c2f19c15b75bd3
TsiamDev <56920806+TsiamDev@users.noreply.github.com>
2019-10-23 15:36:47 +0300

    Add files via upload

---
+++ Assets/Enums.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums
+{
+    //IF YOU ADD HERE also add at Property.cs at the convert_type & convert_sub_type
+    //and also in Game.cs in OnValueChanged
+    public enum PROPERTY_TYPE : int
+    {
+        orchard,
+        livestock,
+        aquaculture,
+        electricity
+    }
+
+    public enum PROPERTY_SUB_TYPE : int
+    {
+        //Orchards
+        pear,
+        apple,
+        plum,
+        citrus,
+        olive,
+        date,
+        fig,
+        orange,
+        pecan,
+        cashews,
+        almonds,
+        //Electricity
+        wind,
+        solar,
+        //Livestock
+        cattle,
+        goat,
+        sheep,
+        swine,
+        poultry,
+        //Aquaculture
+        carp,
+        salmon,
+        tilapia,
+        catfish,
+        microalgea,
+        shrimp,
+        prawn,
+        oyster,
+        mussel
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Enums
+{
+    int land;
+    int water;
+    public int money;
+    public List<Property> properties;
+
+    public Player(int land, int water,int money)
+    {
+        this.money = money;
+        this.land = land;
+        this.water = water;
+        properties = new List<Property>();
+        //properties.Add(new Property("apple_orchard", PROPERTY_TYPE.orchard, 5));
+    }
+    
+}
