15ebc1c74cfb02b62388f5aa49051be0c5b59b53
Tsiamitros <kos.tsiamitros@gmail.com>
2020-01-16 10:32:43 +0200

    Initial

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,26 +23,3 @@ public class Player
     }
     
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : Enums
-{
-    int land;
-    int water;
-    public int money;
-    public List<Property> properties;
-
-    public Player(int land, int water,int money)
-    {
-        this.money = money;
-        this.land = land;
-        this.water = water;
-        properties = new List<Property>();
-        //properties.Add(new Property("apple_orchard", PROPERTY_TYPE.orchard, 5));
-    }
-    
-}
->>>>>>> d602bd9553c61c40b0620c22333b5d3f5996d509
